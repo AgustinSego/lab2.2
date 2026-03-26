60ed6b24466a600aa092ec8ee5eeccd012d1e802
Your Name <you@example.com>
2026-03-26 19:59:12 +0000

    new try

--- list.c
+++ list.c
@@ -90,11 +90,11 @@ void * popBack(List * list) {
 
 void * popCurrent(List * list) {
     Node* borrar = list -> current;
-    //Node* anterior = list -> current-> prev; 
-    //Node* siguiente = list -> current -> next;
+    Node* anterior = list -> current-> prev; 
+    Node* siguiente = list -> current -> next;
     void* dato = list-> current -> data; 
-    if(borrar->prev ==  NULL){
-        list -> head = borrar->next;
+    if(anterior ==  NULL){
+        list -> head = siguente;
         free(borrar);
         return dato;
     }
