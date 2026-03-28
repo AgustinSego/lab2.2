7a813887d469d5375501fe40943950837ebe33eb
Your Name <you@example.com>
2026-03-28 00:58:12 +0000

    new try

--- list.c
+++ list.c
@@ -32,7 +32,11 @@ Node * createNode(void * data) {
 // Recuerda reservar memoria al puntero usando malloc o calloc.
 
 List * createList() {
-     return NULL;
+    List *list = (List *)malloc(sizeof(List));
+    lista->head = NULL;
+    lista->current = NULL;
+    lista->tail = NULL;
+    return lista;
 }
 
 // 2. Programe las funciones void * firstList(List * list) y void * nextList(List * list).
